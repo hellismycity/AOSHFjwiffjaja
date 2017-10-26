da90100140d9f702155af8d70494c4a583fa5030
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-10-26 18:20:27 +1300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,4 +1,12 @@
 const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./app.js');
-Manager.spawn(1)
+const manager = new Discord.ShardingManager('./app.js', {
+  respawn: true,
+  token: process.env.BOT_TOKEN
+});
+
+manager.spawn(2);
+
+manager.on('launch', async (shard) => {
+  console.log(`[STATUS] Spawned shard ${shard.id} / ${manager.totalShards}`)
+});
 
