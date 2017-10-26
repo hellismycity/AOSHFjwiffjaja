44526289755dc4b467b5af4d568764bf31698453
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-10-26 19:37:26 +1300

    Update app.js

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ const moment = require("moment");
 require("moment-duration-format");
 
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  return const embed = new Discord.RichEmbed()
+   const embed = new Discord.RichEmbed()
    embed.setAuthor(`Fergie Stats`)
    embed.addField(`Memory Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
    embed.addField(`Uptime`, `${duration}`, true)
