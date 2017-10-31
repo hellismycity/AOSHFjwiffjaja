5031260a253d859b3532971307831b45c7761088
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-11-01 09:35:09 +1300

    Update app.js

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ async () => {
     return message.reply("Unable to find account. Did you spell it correctly?");
   }
 };
-
+  }
   if (message.content.startsWith(prefix + 'ban')) {
   var reason = message.content.split(' ').slice(2).join(' ');
   let userToBan = message.mentions.users.first()
