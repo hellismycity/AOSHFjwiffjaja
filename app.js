8dcf163097d3c3983160f6d416893fba774a122d
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-11-02 19:08:17 +1300

    Update app.js

--- app.js
+++ app.js
@@ -419,7 +419,7 @@ const embed = new Discord.RichEmbed()
   .addField('Playing', `${target.presence.game === null ? "Not playing anything" : target.presence.game.name}`, true)
   .addField('Created At', `${target.createdAt.toString().substr(0, 15)}`, true)
   .addField('Status', `${target.presence.status}`, true)
-  .addField('Nickname', `${target.displayName}`, true)
+  .addField('Nickname', `${target.nickname}`, true)
   .addField('Bot', `${target.bot}`, true)
   message.channel.send({embed});
     
