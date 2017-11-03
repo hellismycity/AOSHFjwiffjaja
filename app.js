81b6b7cbfda54d3d7df8fba3dbfcd3da36010070
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-11-03 18:27:07 +1300

    Update app.js

--- app.js
+++ app.js
@@ -414,7 +414,7 @@ const status = {
   offline: "Offline/Invisible"
 };
 const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
-  const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+  const member = message.mentions.members.first() || message.guild.members.get(args) || message.member;
   if (!member) return message.reply("Please provide a vaild Mention or USER ID");
   let bot;
   if (member.user.bot === true) {
