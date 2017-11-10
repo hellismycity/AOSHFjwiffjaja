f83962dd1cd0c190457c5fc65073e125116d1a18
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-11-10 18:04:04 +1300

    Update app.js

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ const {get} = require("snekfetch");
         message.channel.send('', {embed: {
     color: 3447003,
     title: 'Cat Image',
-    image: response.body.file
+    image: response.body.url
  
   }
 });
