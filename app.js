6b190d3fcb7491eb32c30dcc6ac26fc9d962d907
hellismycity <32188924+hellismycity@users.noreply.github.com>
2017-11-21 17:50:12 +1300

    Update app.js

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ if (message.content.startsWith(prefix + 'avatar')) {
 } 
   
  if (message.content.startsWith(prefix + 'roles')) {
- message.channel.send(`${message.guild.roles.map(r=>` ${r.name}    ${r.id}`).join('\n')}`, { code: "fix" } )
+ message.channel.send(`${message.guild.roles.map(r=>`# ${r.name}  //  ${r.id}`).join('\n')}`, { code: "md" } )
  }
   
 if (message.content.startsWith(prefix + 'quote')) {
