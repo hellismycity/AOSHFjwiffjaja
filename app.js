a6462147ebd48c762b869bc012fbdb8344af82ee
hellismycity <32188924+hellismycity@users.noreply.github.com>
2018-03-03 10:20:10 +1300

    Update app.js

--- app.js
+++ app.js
@@ -208,9 +208,9 @@ const mentionPrefix = new RegExp(`^<@!?${client.user.id}> `);
   
   if(message.content.startsWith(prefix + "batslap")) {
     message.channel.send("WIP, try again later!")
-/* const Idiot = require('idiotic-api')
+ const Idiot = require('idiotic-api')
 client.API = new Idiot.Client(process.env.image_token, {dev: true})
-message.channel.send(new Discord.MessageAttachment(client.API.batSlap(message.author.displayAvatarURL, message.mentions.users.first().displayAvatarURL), 'batslap.png')); */
+message.channel.send(new Discord.MessageAttachment(client.API.batSlap(message.author.avatarURL, message.mentions.users.first().avatarURL), 'batslap.png'));
   }
   
   if(message.content.startsWith(prefix + "pls")) {
